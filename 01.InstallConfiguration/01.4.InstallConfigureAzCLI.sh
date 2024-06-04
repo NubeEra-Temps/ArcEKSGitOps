fd2715a7e0bd80bcd111f2309e96d07d6833944d
mujahed85 <mujahed.mca@gmail.com>
2024-06-04 15:43:58 +0530

    AZ CLI Supported multiple family of linux

--- 01.InstallConfiguration/01.4.InstallConfigureAzCLI.sh
+++ 01.InstallConfiguration/01.4.InstallConfigureAzCLI.sh
@@ -1,11 +1,32 @@
 # Install Azure CLI
-install_azure_cli() {
-  if ! command -v az &> /dev/null; then
-    echo "Installing Azure CLI..."
-    curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
-  fi
+install_azure_cli_Linux(){
+	echo "Installing Azure CLI..."
+    if [ -f /etc/os-release ]; then
+      . /etc/os-release
+      case $ID in
+	  	debian)
+			curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+		  ;;
+        amzn|fedora)
+			echo -e "[azure-cli]
+			name=Azure CLI
+			baseurl=https://packages.microsoft.com/yumrepos/azure-cli
+			enabled=1
+			gpgcheck=1
+			gpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/azure-cli.repo
+			sudo dnf install azure-cli
+          ;;
+		
+        *)
+          echo "Unsupported distribution for Azure CLI installation"
+          exit 1
+          ;;
+      esac
+    else
+      echo "Unsupported distribution for Azure CLI installation"
+      exit 1
+    fi	
 }
-
 # Configure Azure CLI
 configure_azure_cli(){
 	echo "Configuring Azure CLI..."
