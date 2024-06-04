ade64759b012036335b77feee22c515a57f20603
Syed Mujahed Hussaini <mujahed.mca@gmail.com>
2024-06-04 10:36:12 +0530

    Update 04.2.GitOps.sh

--- 04.Connectivity/04.2.GitOps.sh
+++ 04.Connectivity/04.2.GitOps.sh
@@ -6,16 +6,6 @@
 #     --cluster-type connectedClusters \
 #     --extension-type Microsoft.AzureArcData
 
-az k8s-extension create \
-  --name ArcExtensionFlux \
-  --extension-type microsoft.flux \
-  --cluster-type connectedClusters \
-  --cluster-name "Arc-EKS-Demo" \
-  --resource-group "rgnaresh1135" \
-  --scope cluster \
-  --auto-upgrade-minor-version true
- 
- 
 AZ_ARC_CLUSTER_NAME=Arc-EKS-Demo
 az k8s-extension create \
   --name ArcExtensionFlux \
