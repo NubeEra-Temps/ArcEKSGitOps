80a50aa50b79a5acf45046f499d90274b24dee08
Syed Mujahed Hussaini <mujahed.mca@gmail.com>
2024-06-04 10:38:37 +0530

    Update 04.2.GitOps.sh

--- 04.Connectivity/04.2.GitOps.sh
+++ 04.Connectivity/04.2.GitOps.sh
@@ -6,7 +6,7 @@
 #     --cluster-type connectedClusters \
 #     --extension-type Microsoft.AzureArcData
 
-AZ_ARC_CLUSTER_NAME=Arc-EKS-Demo
+AZ_ARC_CLUSTER_NAME="UniqueName-EKS-Demo"$PAST_PATTERN
 
 az k8s-extension create \
   --name ArcExtensionFlux \
